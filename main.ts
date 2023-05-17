1d477e2e75a92c2cf8cb87927eb873cd49d7f4f7
jcarpon016 <132546629+jcarpon016@users.noreply.github.com>
2023-05-17 21:13:29 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(-25)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("\"POWER\"")
+})
+radio.onReceivedString(function (receivedString) {
+    let receivedstring = 0
+    if (receivedstring == 0) {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(25)
+})
+radio.setGroup(1)
